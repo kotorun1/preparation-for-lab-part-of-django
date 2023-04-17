75a85a802983a76371e6e69acc7cf167cca40900
Merzlyakov Yaroslav <merzlyakovaroslav785@gmail.com>
2023-04-17 12:59:34 +0300

    fgg

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import register, logout, auth, listProductsView, createProductView, detailProductView, createCartView, \
-    getCartView, orderView
+    getCartView, orderView, patchCartView
 
 urlpatterns = [
     path('signup', register),
@@ -10,6 +10,7 @@ urlpatterns = [
     path('product', createProductView),
     path('product/<int:id>', detailProductView),
     path('cart', getCartView),
+    path('cart', patchCartView),
     path('cart/<int:product_id>', createCartView),
     path('order', orderView)
 ]
